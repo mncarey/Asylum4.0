144c8e799426e699ce2347c7c9da10be887540df
dani barajas <dandanbarajas@gmail.com>
2025-11-06 13:22:20 -0800

    fixed rotating hammer script (i hope)

--- Assets/Scripts/RotatingHammers.cs
+++ Assets/Scripts/RotatingHammers.cs
@@ -14,7 +14,7 @@ public class RotatingHammers : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!PauseMenu.isPaused)
         {
